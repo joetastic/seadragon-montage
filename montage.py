808ec05140441dcec7e73e76eb8d28c8fb2bcfb3
Joe Carrafa <joe@carrafa.com>
2012-12-28 17:41:27 -0500

    fixed to work without originals, and double size montage

--- montage.py
+++ montage.py
@@ -14,18 +14,19 @@ def filegenerator():
 
 ifiles = filegenerator()
 
-TILE_SIZE = 76
-IMAGE_SIZE = 64 #num avatars wide
+TILE_SIZE = 150
+IMAGE_SIZE = 128 #num avatars wide
 
 def get_image(size, level, x, y):
+    if size == 1:
+        return ifiles.next()
     if not os.path.exists('%s/%d' % ("avatar_files", level)):
         os.mkdir('%s/%d' % ("avatar_files", level))
     outfile = 'avatar_files/%s/%s_%s.jpg' % (level, x, y)
-    if size == 1:
-        os.link(ifiles.next(), outfile)
-        return outfile
     if level >= 8:
-        subprocess.call(['gm', 'montage', '-tile', '2x2', '-geometry', '%dx%d' % (TILE_SIZE/2, TILE_SIZE/2),
+        subprocess.call(['gm', 'montage',
+                         '-tile', '2x2',
+                         '-geometry', '%dx%d' % (TILE_SIZE/2, TILE_SIZE/2),
                          get_image(size/2, level+1, x*2, y*2),
                          get_image(size/2, level+1, x*2+1, y*2),
                          get_image(size/2, level+1, x*2, y*2+1),
@@ -39,5 +40,8 @@ def get_image(size, level, x, y):
 
 get_image(IMAGE_SIZE, 0, 0, 0)
 with open('avatar.dzi', 'w') as f:
-    x = IMAGE_SIZE*TILE_SIZE*2
-    f.write('<?xml version="1.0" encoding="UTF-8"?><Image TileSize="%d" Overlap="0" Format="jpg" xmlns="http://schemas.microsoft.com/deepzoom/2008"><Size Width="%s" Height="%s"/></Image>' % (TILE_SIZE*2, x, x))
+    x = IMAGE_SIZE*TILE_SIZE/2
+    f.write('<?xml version="1.0" encoding="UTF-8"?>'
+            '<Image TileSize="%d" Overlap="0" Format="jpg" '
+                   'xmlns="http://schemas.microsoft.com/deepzoom/2008">'
+            '<Size Width="%s" Height="%s"/></Image>' % (TILE_SIZE, x, x))
